5493a9f803fe4f8eb49339adfc698c032f891341
Hesong Yang <hesong.yang@foxmail.com>
2026-08-03 11:14:36 +0800

    test: declare minimal lit tool dependencies

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -11,7 +11,7 @@ config.excludes = ["CMakeLists.txt", "lit.cfg.py", "lit.site.cfg.py.in"]
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.ocf_obj_root, "test")
 
-llvm_config.use_default_substitutions()
 llvm_config.add_tool_substitutions(
     ["ocf-opt"], [config.ocf_tools_dir, config.llvm_tools_dir]
 )
+llvm_config.add_tool_substitutions(["FileCheck"], [config.llvm_tools_dir])
